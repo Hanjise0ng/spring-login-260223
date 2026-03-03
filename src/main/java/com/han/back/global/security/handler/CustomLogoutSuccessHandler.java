af51fdde4041f434cb6a7db40a649d3236da42c1
Hanjise0ng <tjdwl3269@gmail.com>
2026-03-04 05:12:34 +0900

    refactor: CustomLogoutSuccessHandler에서 IOException 처리 제거

--- src/main/java/com/han/back/global/security/handler/CustomLogoutSuccessHandler.java
+++ src/main/java/com/han/back/global/security/handler/CustomLogoutSuccessHandler.java
@@ -9,15 +9,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import java.io.IOException;
-
 @RequiredArgsConstructor
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
     private final ObjectMapper objectMapper;
 
     @Override
-    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         HttpResponseUtil.writeResponse(response, objectMapper, BaseResponseStatus.SUCCESS);
     }
 
