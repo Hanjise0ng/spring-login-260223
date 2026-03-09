4f2f01882af6b319aa45ceded5ae26a5508d4a87
Hanjise0ng <tjdwl3269@gmail.com>
2026-03-10 02:33:54 +0900

    refactor: LoginFilter 클라이언트 타입 기본값을 ClientType.WEB으로 변경

--- src/main/java/com/han/back/global/security/filter/LoginFilter.java
+++ src/main/java/com/han/back/global/security/filter/LoginFilter.java
@@ -1,6 +1,7 @@
 package com.han.back.global.security.filter;
 
 import com.han.back.domain.auth.dto.request.SignInRequestDto;
+import com.han.back.domain.user.entity.ClientType;
 import com.han.back.domain.user.entity.Role;
 import com.han.back.global.dto.BaseResponseStatus;
 import com.han.back.global.exception.CustomAuthenticationException;
@@ -91,7 +92,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         log.info("Login Success - UserId: {} | Role: {} | ClientIP: {} | ClientType: {}",
                 id, role.name(), request.getRemoteAddr(),
-                (clientType != null && !clientType.isBlank() ? clientType : "WEB"));
+                (clientType != null && !clientType.isBlank() ? clientType : ClientType.WEB.name()));
     }
 
     private BaseResponseStatus determineLogStatus(AuthenticationException failed) {
