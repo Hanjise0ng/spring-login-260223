f831ceb862e8c6efd24a12631d6dfc82840f5c76
Hanjise0ng <tjdwl3269@gmail.com>
2026-03-18 00:39:07 +0900

    chore: BaseResponseStatus에 import javax.swing.* 제거

--- src/main/java/com/han/back/global/dto/BaseResponseStatus.java
+++ src/main/java/com/han/back/global/dto/BaseResponseStatus.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import javax.swing.*;
-
 @Getter
 @RequiredArgsConstructor
 public enum BaseResponseStatus {
