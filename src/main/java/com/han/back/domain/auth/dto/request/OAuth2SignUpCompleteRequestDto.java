d39d815a26f4bca22b154b70907cafa7a11625fb
Hanjise0ng <tjdwl3269@gmail.com>
2026-06-15 00:07:10 +0900

    refactor: OAuth2SignUpCompleteRequestDto에서 tempToken 필드 제거

--- src/main/java/com/han/back/domain/auth/dto/request/OAuth2SignUpCompleteRequestDto.java
+++ src/main/java/com/han/back/domain/auth/dto/request/OAuth2SignUpCompleteRequestDto.java
@@ -7,15 +7,11 @@ import lombok.Getter;
 @Getter
 public class OAuth2SignUpCompleteRequestDto {
 
-    @NotBlank(message = "소셜 가입 토큰이 필요합니다.")
-    private final String tempToken;
-
     @NotBlank(message = "이메일이 필요합니다.")
     @Email(message = "잘못된 이메일 형식입니다.")
     private final String email;
 
-    public OAuth2SignUpCompleteRequestDto(String tempToken, String email) {
-        this.tempToken = tempToken;
+    public OAuth2SignUpCompleteRequestDto(String email) {
         this.email = email;
     }
 
